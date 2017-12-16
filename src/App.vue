296c85966b246598cc754fa99544a3c64f5f17dc
Shaun Ellis <shaun@sdellis.com>
2017-12-16 16:49:23 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <header>
+      <span>Ash Tag</span>
+    </header>
+    <main>
+      <img src="./assets/logo.png" alt="Ash Tag">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  background-color: #F5F4F1;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #435058;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+
+.alert {
+  background-color: #E1C46E;
+}
+
+.success {
+  background-color: #7FB087;
+}
+
+</style>
